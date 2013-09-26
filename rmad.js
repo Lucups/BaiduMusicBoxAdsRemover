bca21e626d5b7bf440d2175d57d686c1a2298881
Lucups <lucups@gmail.com>
2013-09-26 12:27:20 +0800

    Update:Remove the git lib to GitHub.

---
+++ rmad.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	setTimeout(function(){
+		$(".column4").css("display","none");
+		$("#lrcCol").css("right","0px");
+		$(".column2").css("right","251px");
+	},3000);
+});
